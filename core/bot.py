6feb5514d79d0166ce04d996c0019690550e322f
Connor M. C <42192146+gestalt8003@users.noreply.github.com>
2019-01-30 16:58:37 -0600

    update welcome message

--- core/bot.py
+++ core/bot.py
@@ -7,18 +7,23 @@ client = discord.Client()
 # When bot has loaded
 @client.event
 async def on_ready():
-    print('[Ryze] Logged in as: ', client.user.name, "(" + client.user.id + ")")
+    print('Logged in as:', client.user.name, "(" + client.user.id + ")")
     
     # Update status
-    presence = discord.Game(name=config.PREFIX+"help for commands and usage")
+    presence = discord.Game(name=config.PREFIX+"help for help")
     await client.change_presence(game=presence)
 
-    print('[Ryze] Ready.')
+    print('Ready.')
 
 # Handle message events
 @client.event
 async def on_message(message):
-    if message.content.startswith(config.PREFIX + 'test'):
+    print(message.channel.id)
+    if message.content.startswith(config.PREFIX + 'hello'):
+        await client.send_message(message.channel, 'Hello, ' + message.author.mention + ".")
+    
+    # Count how many messages a user has sent in a channel
+    if message.content.startswith(config.PREFIX + 'count'):
         
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
@@ -28,8 +33,17 @@ async def on_message(message):
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
     
+    # Sleep
     elif message.content.startswith(config.PREFIX + 'sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
+@client.event
+async def on_member_join(member):
+    if config.WELCOME_CHANNEL_ID is not None:
+        server = member.server
+        channel = server.get_channel(config.WELCOME_CHANNEL_ID)
+        await client.send_message(channel, "Welcome to " + server.name + ", " + member.mention + "!")
+
+# Start bot
 client.run(config.TOKEN)
--- core/config.py
+++ core/config.py
@@ -1,3 +1,4 @@
 TOKEN = 'NTIyMjI5OTI5Mzk2NTM1MzA2.DzK9hA.riBtcLzVc41IqO6iPxvl28glPiQ'
 
 PREFIX = '$'
+WELCOME_CHANNEL_ID = '540285616844177443' # None for no welcome message
