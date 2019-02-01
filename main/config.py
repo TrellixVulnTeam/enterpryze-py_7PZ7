700c886852d3d582d06902d0393e96d0f9878e56
Connor M. C <42192146+gestalt8003@users.noreply.github.com>
2019-02-01 11:52:07 -0600

    update config

---
+++ main/config.py
@@ -0,0 +1,16 @@
+# General
+PREFIX = '$'
+ADMIN_PREFIX = PREFIX
+
+STARTUP_MESSAGE = 'I am awake.' # Set to None for no startup message
+SHUTDOWN_MESSAGE = 'Going to sleep...' # Set to None for no shutdown message
+
+# Welcome message
+WELCOME_MESSAGE = True
+DEFAULT_WELCOME_CHANNEL = 'welcome' 
+DEFAULT_BOT_CHANNEL = 'bot'
+
+# Voting
+DEFAULT_VOTE_TIME = 1 # seconds
+AYE = '✅'
+NAY = '❌'
